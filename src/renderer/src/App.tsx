480829f6a377b02a793308b49cb5f77bcbca40f3
Manjul Tamrakar <manjultamrakar4@gmail.com>
2024-06-17 21:33:52 +0545

    added floating note title

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -5,7 +5,8 @@ import {
   DraggableTopBar,
   ActionButtonRow,
   NotePreviewList,
-  MarkdownEditor
+  MarkdownEditor,
+  FloatingNoteTitle
 } from '@/components'
 
 const App = () => {
@@ -18,6 +19,7 @@ const App = () => {
           <NotePreviewList className="mt-3 space-y-1" />
         </Sidebar>
         <Content className="border-l bg-zinc-900/50 border-l-white/20">
+          <FloatingNoteTitle className="pt-2" />
           <MarkdownEditor />
         </Content>
       </RootLayout>
