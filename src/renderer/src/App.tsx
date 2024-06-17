e1581688b1fb6bb726d305d1d952ff88b4d8bdd7
Manjul Tamrakar <manjultamrakar4@gmail.com>
2024-06-17 19:09:03 +0545

    first commit

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <span className="text-4xl text-blue-500">hello Electron</span>
+    </div>
+  )
+}
+
+export default App
